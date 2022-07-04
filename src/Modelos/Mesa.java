03d11877b5a65677071ba9524a55e5b79f68ba92
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2022-07-04 17:58:33 -0300

    mesa

--- src/Modelos/Mesa.java
+++ src/Modelos/Mesa.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelos;
 
-/**
- *
- * @author Usuario
- */
+import java.time.LocalDate;
+import java.util.HashSet;
+
 public class Mesa {
+    
+    
+package Modelos;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+
+public class Mesa {
+    
+    private int idMesa; 
+    private Reserva reserva;
+    private int capacidad;
+    private boolean ocupada;
+    private LocalDate fecha;
+    private HashSet <Producto> listaProducto;
+
+    public Mesa() {
+    }
+
+    public Mesa(Reserva reserva, int capacidad, boolean ocupada, LocalDate fecha, HashSet<Producto> listaProducto) {
+        this.idMesa = idMesa;
+        this.reserva = reserva;
+        this.capacidad = capacidad;
+        this.ocupada = ocupada;
+        this.fecha = fecha;
+        this.listaProducto = listaProducto;
+    }
+
+    public Mesa(int idMesa, Reserva reserva, int capacidad, boolean ocupada, LocalDate fecha, HashSet<Producto> listaProducto) {
+        this.idMesa = idMesa;
+        this.reserva = reserva;
+        this.capacidad = capacidad;
+        this.ocupada = ocupada;
+        this.fecha = fecha;
+        this.listaProducto = listaProducto;
+    }
+
+    public int getIdMesa() {
+        return idMesa;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public HashSet<Producto> getListaProducto() {
+        return listaProducto;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setListaProducto(HashSet<Producto> listaProducto) {
+        this.listaProducto = listaProducto;
+    }
+    
+    
     
 }
