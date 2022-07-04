931556f201f3119a10432fda6a4c4dee0b518b14
samirabdala <samirabdala10@gmail.com>
2022-07-04 18:03:13 -0300

    Entidad Pedido

--- src/Modelos/Pedido.java
+++ src/Modelos/Pedido.java
@@ -4,10 +4,64 @@
  */
 package Modelos;
 
+import java.util.*;
+
 /**
  *
  * @author Samir
  */
 public class Pedido {
+    
+    private int idPedido;
+    private HashSet <Producto> listaProducto;
+    private Mesa mesa;
+    private double subTotal;
+
+    public Pedido() {
+    }
+
+    public Pedido(HashSet<Producto> listaProducto, Mesa mesa, double subTotal) {
+        this.listaProducto = listaProducto;
+        this.mesa = mesa;
+        this.subTotal = subTotal;
+    }
+
+   
+    public Pedido(int idPedido , HashSet<Producto> listaProducto, Mesa mesa, double subTotal) {
+        this.idPedido = idPedido;
+        this.listaProducto = listaProducto;
+        this.mesa = mesa;
+        this.subTotal = subTotal;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+
+    public HashSet<Producto> getListaProducto() {
+        return listaProducto;
+    }
+
+    public void setListaProducto(HashSet<Producto> listaProducto) {
+        this.listaProducto = listaProducto;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+    
     
 }
