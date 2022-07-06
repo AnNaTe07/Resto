1b7775e76394bcb3d8470dd017796899c8b9b26a
AnNaTe07 <102766959+AnNaTe07@users.noreply.github.com>
2022-07-05 21:07:29 -0300

    MesaData y ReservaData

---
+++ src/Data/MesaData.java
@@ -0,0 +1,171 @@
+
+package Data;
+
+import Modelos.Mesa;
+import Modelos.Pedido;
+import Modelos.Producto;
+import Modelos.Reserva;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class MesaData {
+    
+    private Connection con = null;      
+
+    public MesaData(Conexion conexion) {
+
+        con = conexion.getConexion();            
+
+    }
+    
+    public boolean agregarMesa(Mesa mesa) {
+        boolean exito = true;
+        String sql = "INSERT INTO mesa(idMesa, capacidad, ocupada, fecha, activo, estado) VALUES (? ,? , ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, mesa.getIdMesa());
+            ps.setInt(2, mesa.getCapacidad());
+            ps.setBoolean(3, mesa.isOcupada());
+            ps.setDate(4,Date.valueOf(mesa.getFecha()));
+            ps.setBoolean(5,mesa.isActivo());
+            ps.setString(6 ,mesa.getEstado());
+             
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                mesa.setIdMesa(rs.getInt(1));
+            } else {
+
+                exito = false;
+            }
+
+            ps.close();
+
+        } catch (SQLException e) {
+            if (e instanceof java.sql.SQLIntegrityConstraintViolationException) {
+                exito=false;
+                JOptionPane.showMessageDialog(null, "Ya existe una mesa registrada con ese número");
+            } else {
+
+                JOptionPane.showMessageDialog(null, "Error de sintaxis " + e);
+
+            }
+        }
+
+        return exito;
+    }
+    public boolean borrarMesa(int id) {
+
+        boolean borrado = false;
+        String sql = "UPDATE mesa SET activo = 0 WHERE idMesa = ?";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            if (ps.executeUpdate() != 0) {
+
+                borrado = true;
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de sintaxis ");
+        }
+
+        return borrado;
+    }
+ 
+    public boolean modificarMesa(Mesa mesa) {
+        boolean exito = false;
+
+        String sql = "UPDATE  mesa SET idMesa = ?, capacidad = ?, ocupada = ?, fecha = ?, activo = ?, estado = ? WHERE idMesa = ? ";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, mesa.getIdMesa());
+            ps.setInt(2, mesa.getCapacidad());
+            ps.setBoolean(3, mesa.isOcupada());
+            ps.setDate(4,Date.valueOf(mesa.getFecha()));
+            ps.setBoolean(5,mesa.isActivo());
+            ps.setString(6 ,mesa.getEstado());
+            if (ps.executeUpdate() != 0) {
+
+                exito = true;
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo modificar la mesa");
+        }
+
+        return exito;
+
+    }
+
+    public ArrayList<Mesa> obtenerMesas() {
+        ArrayList<Mesa> listaMesa = new ArrayList();       
+
+        try {
+            String sql = "SELECT * FROM mesa WHERE activo = 1;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+            
+            Mesa mesa;
+            while (resultSet.next()) {
+                mesa = new Mesa();
+                mesa.setIdMesa(resultSet.getInt("idMesa"));
+                mesa.setCapacidad(resultSet.getInt("capacidad"));
+                mesa.setOcupada(resultSet.getBoolean("ocupada"));
+                mesa.setFecha(resultSet.getDate("fecha").toLocalDate());
+                mesa.setEstado(resultSet.getString("estado"));
+
+                listaMesa.add(mesa);
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se puede obtener las mesas");
+        }
+        return listaMesa;
+    }
+    
+    public Mesa obtenerMesaxId(int id) {
+
+        Mesa mesa = null;
+
+        try {
+            String sql = "SELECT * FROM mesa WHERE idMesa = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                mesa = new Mesa();
+                mesa.setIdMesa(resultSet.getInt("idMesa"));
+                mesa.setCapacidad(resultSet.getInt("capacidad"));
+                mesa.setOcupada(resultSet.getBoolean("ocupada"));
+                mesa.setFecha(resultSet.getDate("fecha").toLocalDate());
+                mesa.setActivo(resultSet.getBoolean("activo"));
+                mesa.setEstado(resultSet.getString("estado"));
+
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al obtener alumnos");
+        }
+
+        return mesa;
+    }    
+   
+}
---
+++ src/Data/ReservaData.java
@@ -0,0 +1,175 @@
+
+package Data;
+
+import Modelos.Reserva;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class ReservaData {
+    
+     private Connection con = null;      
+
+    public ReservaData(Conexion conexion) {
+
+        con = conexion.getConexion();            
+
+    }
+    
+    public boolean agregarReserva(Reserva reserva) {
+        boolean exito = true;
+        String sql = "INSERT INTO reserva(idReserva, nombre, dni, fecha, hora, idMesa, activo) VALUES (?, ? ,? , ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, reserva.getIdReserva());
+            ps.setString(2, reserva.getNombre());
+            ps.setInt(3, reserva.getDni());
+            ps.setDate(4, (Date) reserva.getFecha());
+            ps.setTime(5,reserva.getHora());
+            ps.setInt(6, reserva.getIdMesa());
+            ps.setBoolean(7,reserva.isActivo());
+            
+             
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                reserva.setIdReserva(rs.getInt(1));
+            } else {
+
+                exito = false;
+            }
+
+            ps.close();
+
+        } catch (SQLException e) {
+            if (e instanceof java.sql.SQLIntegrityConstraintViolationException) {
+                exito=false;
+                JOptionPane.showMessageDialog(null, "Ya existe una reserva registrada con ese número de dni");
+            } else {
+
+                JOptionPane.showMessageDialog(null, "Error de sintaxis " + e);
+
+            }
+        }
+
+        return exito;
+    }
+    public boolean borrarReserva(int id) {
+
+        boolean borrado = false;
+        String sql = "UPDATE reserva SET activo = 0 WHERE idReserva = ?";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            if (ps.executeUpdate() != 0) {
+
+                borrado = true;
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de sintaxis ");
+        }
+
+        return borrado;
+    }
+ 
+    public boolean modificarReserva(Reserva reserva) {
+        boolean exito = false;
+
+        String sql = "UPDATE reserva SET idReserva =?, nombre=?, dni=?, fecha=?, hora=?, idMesa=?, activo=? WHERE idReserva = ? ";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, reserva.getIdReserva());
+            ps.setString(2, reserva.getNombre());
+            ps.setInt(3, reserva.getDni());
+            ps.setDate(4, (Date) reserva.getFecha());
+            ps.setTime(5,reserva.getHora());
+            ps.setInt(6, reserva.getIdMesa());
+            ps.setBoolean(7,reserva.isActivo());
+            
+            if (ps.executeUpdate() != 0) {
+
+                exito = true;
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo modificar la reserva");
+        }
+
+        return exito;
+
+    }
+
+    public ArrayList<Reserva> obtenerReservas() {
+        ArrayList<Reserva> listaReservas = new ArrayList();       
+
+        try {
+            String sql = "SELECT * FROM reserva WHERE activo = 1;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+            
+            Reserva reserva;
+            while (resultSet.next()) {
+                reserva = new Reserva();
+                reserva.setIdReserva(resultSet.getInt("isReserva"));
+                reserva.setNombre(resultSet.getString("nombre"));
+                reserva.setDni(resultSet.getInt("dni"));
+                reserva.setFecha(resultSet.getDate("fecha"));
+                reserva.setHora(resultSet.getTime("hora"));
+                reserva.setIdMesa(resultSet.getInt("idMesa"));
+                reserva.setActivo(resultSet.getBoolean("activo"));
+
+                listaReservas.add(reserva);
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "No se puede obtener las reservas");
+        }
+        return listaReservas;
+    }
+    
+    public Reserva obtenerReservaxId(int id) {
+
+        Reserva reserva = null;
+
+        try {
+            String sql = "SELECT * FROM mesa WHERE idMesa = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()) {
+                 reserva = new Reserva();
+                reserva.setIdReserva(resultSet.getInt("isReserva"));
+                reserva.setNombre(resultSet.getString("nombre"));
+                reserva.setDni(resultSet.getInt("dni"));
+                reserva.setFecha(resultSet.getDate("fecha"));
+                reserva.setHora(resultSet.getTime("hora"));
+                reserva.setIdMesa(resultSet.getInt("idMesa"));
+                reserva.setActivo(resultSet.getBoolean("activo"));
+
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al obtener reserva");
+        }
+
+        return reserva;
+    }    
+   
+}
